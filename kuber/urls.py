59a9a2cf60d17cd53e15275fe71a28e00d74fb5a
xbw1220 <30739769+xbw1220@users.noreply.github.com>
2021-11-23 21:29:47 +0800

    添加node、pv

--- kuber/urls.py
+++ kuber/urls.py
@@ -4,6 +4,8 @@ from kuber import views
 urlpatterns = [
     # path('admin/', admin.site.urls),
     re_path('^node/$', views.node, name="node"),
+    re_path('^node_api/$', views.node_api, name="node_api"),
     re_path('^namespace/$', views.namespace, name="namespace"),
     re_path('^pv/$', views.pv, name="pv"),
+    re_path('^pv_api/$', views.pv_api, name="pv_api"),
 ]
--- kuber/views.py
+++ kuber/views.py
@@ -1,4 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import JsonResponse, QueryDict
+from kubernetes import client, config
+import os, hashlib, random
+from k8s import k8s
 
 
 # Create your views here.
@@ -6,8 +10,163 @@ def node(request):
     return render(request, 'k8s/node.html')
 
 
+def node_api(request):
+    if request.method == 'GET':
+        auth_type = request.session.get("auth_type")
+        token = request.session.get("token")
+        k8s.load_auth_config(auth_type, token)
+        core_api = client.CoreV1Api()
+        data = []
+        try:
+            for node in core_api.list_node_with_http_info()[0].items:
+                name = node.metadata.name
+                labels = node.metadata.labels
+                status = node.status.conditions[-1].status
+                scheduler = ("是" if node.spec.unschedulable is None else "否")
+                cpu = node.status.capacity['cpu']
+                memory = node.status.capacity['memory']
+                kebelet_version = node.status.node_info.kubelet_version
+                cri_version = node.status.node_info.container_runtime_version
+                create_time = node.metadata.creation_timestamp
+                node = {"name": name, "labels": labels, "status": status,
+                        "scheduler": scheduler, "cpu": cpu, "memory": memory,
+                        "kebelet_version": kebelet_version, "cri_version": cri_version,
+                        "create_time": create_time}
+                data.append(node)
+            code = 0
+            msg = "获取数据成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有访问权限"
+            else:
+                msg = "获取数据失败"
+        count = len(data)
+        page = int(request.GET.get('page', 1))
+        # limit = int(request.GET.get('name'))
+        limit = count
+        start = (page - 1) * limit
+        end = page * limit
+        data = data[start:end]
+        res = {'code': code, 'msg': msg, 'count': count, 'data': data}
+        return JsonResponse(res)
+    elif request.method == "DELETE":
+        request_data = QueryDict(request.body)
+        # print(request_data)
+        name = request_data.get("name")
+        auth_type = request.session.get("auth_type")
+        token = request.session.get("token")
+        k8s.load_auth_config(auth_type, token)
+        core_api = client.CoreV1Api()
+        data = []
+        try:
+            core_api.delete_namespace(name)
+            code = 0
+            msg = "删除成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有删除权限！"
+            else:
+                msg = "删除失败！"
+        res = {'code': code, 'msg': msg}
+        return JsonResponse(res)
+
+
 def namespace(request):
     return render(request, 'k8s/namespace.html')
+
+
+def pv_api(request):
+    if request.method == 'GET':
+        auth_type = request.session.get("auth_type")
+        token = request.session.get("token")
+        k8s.load_auth_config(auth_type, token)
+        core_api = client.CoreV1Api()
+        data = []
+        try:
+            for pv in core_api.list_persistent_volume().items:
+                name = pv.metadata.name
+                capacity = pv.spec.capacity["storage"]
+                access_modes = pv.spec.access_modes
+                reclaim_policy = pv.spec.persistent_volume_reclaim_policy
+                status = pv.status.phase
+                if pv.spec.claim_ref is not None:
+                    pvc_ns = pv.spec.claim_ref.namespace
+                    pvc_name = pv.spec.claim_ref.name
+                    pvc = "%s / %s" % (pvc_ns, pvc_name)
+                else:
+                    pvc = "未绑定"
+                storage_class = pv.spec.storage_class_name
+                create_time = pv.metadata.creation_timestamp
+                pv = {"name": name, "capacity": capacity, "access_modes": access_modes,
+                      "reclaim_policy": reclaim_policy, "status": status, "pvc": pvc,
+                      "storage_class": storage_class, "create_time": create_time}
+                data.append(pv)
+            code = 0
+            msg = "获取数据成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有访问权限"
+            else:
+                msg = "获取数据失败"
+        count = len(data)
+        page = int(request.GET.get('page', 1))
+        # limit = int(request.GET.get('name'))
+        limit = count
+        start = (page - 1) * limit
+        end = page * limit
+        data = data[start:end]
+        res = {'code': code, 'msg': msg, 'count': count, 'data': data}
+        return JsonResponse(res)
+    elif request.method == "POST":
+        name = request.POST.get("name", None)
+        capacity = request.POST.get("capacity", None)
+        access_mode = request.POST.get("access_mode", None)
+        storage_type = request.POST.get("storage_type", None)
+        server_ip = request.POST.get("server_ip", None)
+        mount_path = request.POST.get("mount_path", None)
+
+        body = client.V1PersistentVolume(
+            api_version="v1",
+            kind="PersistentVolume",
+            metadata=client.V1ObjectMeta(name=name),
+            spec=client.V1PersistentVolumeSpec(
+                capacity={'storage': capacity},
+                access_modes=[access_mode],
+                nfs=client.V1NFSVolumeSource(
+                    server=server_ip,
+                    path="/ifs/kubernetes/%s" % mount_path
+                )
+            )
+        )
+        core_api.create_persistent_volume(body=body)
+    elif request.method == "DELETE":
+        request_data = QueryDict(request.body)
+        # print(request_data)
+        name = request_data.get("name")
+        auth_type = request.session.get("auth_type")
+        token = request.session.get("token")
+        k8s.load_auth_config(auth_type, token)
+        core_api.delete_persistent_volume(name=name)
+        data = []
+        try:
+            core_api.delete_namespace(name)
+            code = 0
+            msg = "删除成功"
+        except Exception as e:
+            code = 1
+            status = getattr(e, "status")
+            if status == 403:
+                msg = "没有删除权限！"
+            else:
+                msg = "删除失败！"
+        res = {'code': code, 'msg': msg}
+        return JsonResponse(res)
 
 
 def pv(request):
